8c130fc8132c77e60000d34e44d4745ea63150c6
Chocks <chocks@outlook.com>
2016-11-12 22:16:18 -0600

    py concurrent user sessions

--- helper/selenium_test.py
+++ helper/selenium_test.py
@@ -1,4 +1,5 @@
 import time
+import pp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,6 +10,14 @@ import sys
 import random
 
 sys.path.insert(1, "../bin")
+
+def worker(name):
+    print 'Worker' + str(name)
+    driver = init_driver()
+    lookup(driver, "user-"+str(random.randint(1,10000)))
+    time.sleep(5)
+    driver.quit()
+    return
 
 def init_driver():
     driver = webdriver.Firefox()
@@ -24,14 +33,37 @@ def lookup(driver, query):
         user.send_keys(Keys.RETURN)
         chat = driver.wait.until(EC.presence_of_element_located((By.ID, "box")))
         for i in range(0, 5):
-            chat.send_keys("sometext"+str(random.random()))
+            chat.send_keys("sometext"+str(random.randint(0,10000)))
             chat.send_keys(Keys.RETURN)
         # assert "Chocks" in driver.page_source
     except TimeoutException:
         print("Elements not found..")
 
+def mp_handler():
+    job_server = pp.Server(8, ppservers=())
+    jobs = []
+
+    for index in range(0, 10):
+        jobs.append(job_server.submit(worker, (index,), 
+            modules=(
+                    "time",
+                    "selenium",
+                    "sys",
+                    "random",
+                    "from selenium import webdriver",
+                    "from selenium.webdriver.common.by import By",
+                    "from selenium.webdriver.support.ui import WebDriverWait",
+                    "from selenium.webdriver.support import expected_conditions as EC",
+                    "from selenium.common.exceptions import TimeoutException",
+                    "from selenium.webdriver.common.keys import Keys"
+            ),
+        globals=globals()
+        ))
+    for job in jobs:
+        result = job()
+        if result:
+            break
+
 if __name__ == "__main__":
-    driver = init_driver()
-    lookup(driver, "user"+str(random.random()))
-    time.sleep(5)
-    driver.quit()
+    mp_handler()
+
